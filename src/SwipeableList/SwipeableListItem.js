bae030269fee8396487d85e51edc66ed1db86dbb
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-02-19 17:40:29 +0000

    adding drag & drop feature and event listeners

--- src/SwipeableList/SwipeableListItem.js
+++ src/SwipeableList/SwipeableListItem.js
@@ -25,6 +25,32 @@ export default class SwipeableListItem extends React.Component {
         this.onDragEnd = this.onDragEnd.bind(this);
         this.updatePosition = this.updatePosition.bind(this);
         this.onSwiped= this.onSwiped.bind(this);
+
+        // Drag & Drop
+        dragStartX = 0;
+        left = 0;
+        dragged = false;
+
+
+        // Mouse event drag listener
+        onDragStartMouse(event) {
+            this.onDragStart(event.clientX);
+            window.addEventListener('mousemove', this.onMouseMove);
+        }
+
+        // Touch event drag listener
+        onDragStartTouch(event) {
+            const touch = event.targetTouches[0];
+            this.onDragStart(touch.clientX);
+            window.addEventListener('touchmove', this.onTouchMove);
+        }
+
+        // onDragStart method
+        onDragStart(clientX) {
+            this.dragged = true;
+            this.dragStartX = clientX;
+            requestAnimationFrame(this.updatePosition);
+        }
     }
     render() {
         return (
@@ -33,7 +59,14 @@ export default class SwipeableListItem extends React.Component {
                 <div className='Background' ref={div => (this.background = div)}>
                     <span>Delete</span>
                 </div>
-                <div className='ListItem' ref={div => (this.listElement = div)}>{this.props.children}</div>
+                <div 
+                    className='ListItem' 
+                    ref={div => (this.listElement = div)}
+                    onMouseDown={this.onDragStartMouse}
+                    onTouchStart={this.onDragStartTouch}
+                >
+                    {this.props.children}
+                </div>
             </div>
         </>
         )
